661f5880436868a194c56a76af848b6aee0cc314
Aaron Sadler <aaron@aaronsadler.co.uk>
2022-02-15 22:12:41 +0000

    Finally figured out how to intercept the backoffice route

---
+++ Our.Umbraco.MaskBackofficeV9/Composers/OverrideBackofficeRoute.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Our.Umbraco.MaskBackofficeV9.Controllers;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Hosting;
+using Umbraco.Cms.Web.Common.ApplicationBuilder;
+using Umbraco.Extensions;
+
+namespace Our.Umbraco.MaskBackofficeV9.Composers;
+
+public class OverrideBackofficeRoute : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Services.Configure<UmbracoPipelineOptions>(options =>
+        {
+            options.AddFilter(new UmbracoPipelineFilter(nameof(MaskBackofficeController))
+            {
+                Endpoints = app => app.UseEndpoints(endpoints =>
+                {
+                    var globalSettings = app.ApplicationServices
+                        .GetRequiredService<IOptions<GlobalSettings>>().Value;
+                    var hostingEnvironment = app.ApplicationServices
+                        .GetRequiredService<IHostingEnvironment>();
+                    var rootSegment = $"{globalSettings.GetBackOfficePath(hostingEnvironment)}";
+                    endpoints.MapUmbracoRoute<MaskBackofficeController>(
+                        rootSegment,
+                        string.Empty,
+                        string.Empty,
+                        "Default",
+                        includeControllerNameInRoute: false,
+                        constraints:
+
+                        // Limit the action/id to only allow characters - this is so this route doesn't hog all other
+                        // routes like: /umbraco/channels/word.aspx, etc...
+                        // (Not that we have to worry about too many of those these days, there still might be a need for these constraints).
+                        new
+                        {
+                            action = @"[a-zA-Z]*",
+                            id = @"[a-zA-Z]*"
+                        });
+                })
+            });
+        });
+    }
+}
---
+++ Our.Umbraco.MaskBackofficeV9/Controllers/MaskBackofficeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace Our.Umbraco.MaskBackofficeV9.Controllers;
+
+public class MaskBackofficeController : UmbracoController
+{
+    private readonly IRuntimeState _runtimeState;
+
+    public MaskBackofficeController(IRuntimeState runtimeState)
+    {
+        _runtimeState = runtimeState;
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Default()
+    {
+        // Check if we not are in an run state, if so we need to redirect
+        if (_runtimeState.Level != RuntimeLevel.Run)
+        {
+            return Redirect("/");
+        }
+
+        return NotFound();
+    }
+}
