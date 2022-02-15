e175fec9dd5bb14935c93be8833de61ad4fd65a6
Aaron Sadler <aaron@aaronsadler.co.uk>
2022-02-15 23:11:49 +0000

    Got it to return the default Umbraco not found view

---
+++ Our.Umbraco.MaskBackofficeV9/ActionResults/NotFoundResult.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Our.Umbraco.MaskBackofficeV9.ActionResults
+{
+    /// <summary>
+    /// Returns the Umbraco not found result
+    /// </summary>
+    public class NotFoundResult : IActionResult
+    {
+        private readonly string _message;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotFoundResult"/> class.
+        /// </summary>
+        public NotFoundResult(string message = null)
+        {
+            _message = message;
+        }
+
+        /// <inheritdoc/>
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            HttpResponse response = context.HttpContext.Response;
+
+            response.Clear();
+
+            response.StatusCode = StatusCodes.Status404NotFound;
+
+            var viewResult = new ViewResult
+            {
+                ViewName = "~/umbraco/UmbracoWebsite/NotFound.cshtml"
+            };
+            
+            await viewResult.ExecuteResultAsync(context);
+        }
+    }
+}
--- Our.Umbraco.MaskBackofficeV9/Controllers/MaskBackofficeController.cs
+++ Our.Umbraco.MaskBackofficeV9/Controllers/MaskBackofficeController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Web.Common.Controllers;
 
 namespace Our.Umbraco.MaskBackofficeV9.Controllers;
@@ -9,10 +12,12 @@ namespace Our.Umbraco.MaskBackofficeV9.Controllers;
 public class MaskBackofficeController : UmbracoController
 {
     private readonly IRuntimeState _runtimeState;
+    private readonly IUmbracoContextFactory _umbracoContextFactory;
 
-    public MaskBackofficeController(IRuntimeState runtimeState)
+    public MaskBackofficeController(IRuntimeState runtimeState, IUmbracoContextFactory umbracoContextFactory)
     {
         _runtimeState = runtimeState;
+        this._umbracoContextFactory = umbracoContextFactory;
     }
 
     [HttpGet]
@@ -26,5 +31,13 @@ public class MaskBackofficeController : UmbracoController
         }
 
         return NotFound();
+    }
+    
+    /// <summary>
+        /// Before the controller executes we will handle redirects and not founds
+        /// </summary>
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        context.Result = new Our.Umbraco.MaskBackofficeV9.ActionResults.NotFoundResult();
     }
 }
