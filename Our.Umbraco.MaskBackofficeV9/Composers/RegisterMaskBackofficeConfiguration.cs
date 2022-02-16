735242abfee4e1e4408fe68678c04731b1db7f42
Aaron Sadler <aaron@aaronsadler.co.uk>
2022-02-16 21:44:43 +0000

    Added configurations

---
+++ Our.Umbraco.MaskBackofficeV9/Composers/RegisterMaskBackofficeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Our.Umbraco.MaskBackofficeV9.Configurations;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Our.Umbraco.MaskBackofficeV9.Composers;
+
+public class RegisterMaskBackofficeConfiguration : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Services.Configure<OurUmbracoMaskBackoffice>(
+            builder.Config.GetSection("OurUmbracoMaskBackoffice"));
+    }
+}
---
+++ Our.Umbraco.MaskBackofficeV9/Configurations/OurUmbracoMaskBackoffice.cs
@@ -0,0 +1,10 @@
+﻿namespace Our.Umbraco.MaskBackofficeV9.Configurations;
+
+public class OurUmbracoMaskBackoffice
+{
+    public bool Enabled { get; set; } = false;
+    public string ViewName { get; set; } = "MaskBackofficeNotFound.cshtml";
+    public bool UseRedirect { get; set; } = false;
+    public string RedirectUrl { get; set; }
+    public string[] Domains { get; set; }
+}
